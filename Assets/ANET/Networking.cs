b1ac8d9b592732fc40320fddc26136788c9313ac
Matheus Crivellari <matheus.acrivellari@gmail.com>
2018-02-01 01:39:22 -0200

    Criada tela para escolha do modo de jogo. Craida tela de preload da partida. Criada interface de networking baseada em SocketIO. Adicionado metodos Connect e Play.

---
+++ Assets/ANET/Networking.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+namespace ANET
+{
+
+    namespace Networking
+    {
+
+        public class GameMode
+        {
+            public static readonly uint VR = 0;
+            public static readonly uint MOBILE = 1;
+        }
+
+        public class Networking : MonoBehaviour
+        {
+            #region Static Fields
+            static private Networking _instance;
+            #endregion
+
+            #region Privates Fields
+            [SerializeField]
+            private SocketIOComponent io = null;
+            #endregion
+
+            #region Public Properties
+            static public Networking Instance
+            {
+                get
+                {
+                    return _instance;
+                }
+            }
+            #endregion
+
+            #region Unity Interface
+            private void Awake()
+            {
+                _instance = this;
+            }
+
+            private void Start()
+            {
+                Instance.Connect();
+            }
+            #endregion
+
+            #region Public Interface
+            public void Connect()
+            {
+                if (io)
+                {
+                    io.Connect();
+                }
+            }
+
+            public void Play(uint gameMode)
+            {
+                JSONObject payload = new JSONObject();
+                payload.AddField("action", "makematch");
+
+                if(gameMode == GameMode.MOBILE)
+                {
+                    payload.AddField("type", "mobile");
+                    io.Emit("action",payload);
+                }else if(gameMode == GameMode.VR)
+                {
+                    payload.AddField("type", "vr");
+                    io.Emit("action",payload);
+                }
+                
+            }
+            #endregion
+
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/Helpers/ClickToPlayWithVR.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ANET.Networking;
+
+public class ClickToPlayWithVR : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject gameModePanel = null;
+
+    [SerializeField]
+    private GameObject preloadPanel = null;
+
+    public void Click()
+    {
+        gameModePanel.SetActive(false);
+        preloadPanel.SetActive(true);
+        Networking.Instance.Play(GameMode.VR);
+    }
+}
